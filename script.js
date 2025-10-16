8ccd11f8b7c14529627f69cc1087a7d49eeeb1d7
MorioWada <moriowadaw816@gmail.com>
2025-10-16 12:52:54 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+// Simple smooth scroll for navigation
+const navLinks = document.querySelectorAll('nav a');
+navLinks.forEach(link => {
+    link.addEventListener('click', function(e) {
+        const href = this.getAttribute('href');
+        if (href.startsWith('#')) {
+            e.preventDefault();
+            document.querySelector(href).scrollIntoView({ behavior: 'smooth' });
+        }
+    });
+});
